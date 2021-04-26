ffc007f06253bb0134e52015a6a526f1cd213ea0
suresh4sparktics <suresh.p@sparktics@gmail.com>
2021-04-26 19:56:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import Banner from "./components/Banner";
+import Header from "./components/Header";
+import "./styles/App.scss";
 
 function App() {
+  useEffect(() => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Banner />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="container">
+        <div className="row v-center space-between">
+          <div className="logo">
+            <a href="/">AGENCY.</a>
+          </div>
+          <nav className="nav">
+            <span></span>
+            <span></span>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
